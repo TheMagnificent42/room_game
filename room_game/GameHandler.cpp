0c0dacd01edb1cc2d42624aa1be11003adc44e26
TheMagnificent42 <themagnificent42@gmail.com>
2017-03-10 13:14:06 -0500

    Update GameHandler.cpp

--- room_game/GameHandler.cpp
+++ room_game/GameHandler.cpp
@@ -1,1 +1,19 @@
-//manages user input, current player position, and passing to rooms contents
+//manages user input, saving, current player position, and passing to rooms' contents
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class GameHandler
+{
+  private:
+    string saveFile;
+    
+  public:
+    GameHandler(string);
+};
+
+GameHandler::GameHandler(string loadFile)
+{
+  saveFile = loadFile;
+}
