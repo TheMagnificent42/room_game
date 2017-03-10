fb6a5e85184e00664a1e2c6b73fdb97427010455
TheMagnificent42 <themagnificent42@gmail.com>
2017-03-10 13:40:31 -0500

    Update Room.cpp

--- room_game/Room.cpp
+++ room_game/Room.cpp
@@ -1,1 +1,21 @@
 //class for the rooms
+#include <fstream>
+
+using namespace std;
+
+class Room
+{
+  private:
+    fstream saveFile;
+    ObjectInteractable contents[];
+  
+  public:
+    Room(string);
+};
+
+Room::Room(string loadFile)
+{
+  saveFile = load(loadFile);
+  
+  
+}
